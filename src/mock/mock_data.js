e1ae011f46fe6b6fa1e942a63767bdf2bfccba8c
ztnb66 <119243209+ztnb66@users.noreply.github.com>
2025-09-27 12:23:17 +0800

    chore: 更新导航数据 - 2025/9/27 12:23:16

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -481,6 +481,6 @@ export const mockData = {
       ]
     }
   ],
-  "title": "猫猫导航🐱",
-  "_fileSha": "782bd4a5e3de31d5171bc16c726b5b2b0b2c2002"
+  "title": "techtao导航",
+  "_fileSha": "8d7abcaecd8cb14b56158b8dad478a98d00c51d5"
 }
